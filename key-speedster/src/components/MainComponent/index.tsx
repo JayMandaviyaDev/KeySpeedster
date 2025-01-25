10547f9c4eca97a2c929dd9c0117562369ca56a3
Jaykumar Mandaviya <jay1704mandaviya@gmail.com>
2025-01-25 16:53:47 +0530

    added some utils

--- key-speedster/src/components/MainComponent/index.tsx
+++ key-speedster/src/components/MainComponent/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
-
+"use client";
+import React, { useEffect, useState } from "react";
+import generateRandom from "@/utils/utils";
+import paragraphs from "../../utils/paragraphs.json";
 export default function MainContent() {
+  const [paragraph, setParagraph] = useState<string>("");
+  useEffect(() => {
+    setParagraph(paragraphs[generateRandom(1, 10).toString()]);
+  }, []);
+
   return (
-    <div>index</div>
-  )
+    <div>
+      <div className="m-4 px-5">{paragraph}</div>
+    </div>
+  );
 }
